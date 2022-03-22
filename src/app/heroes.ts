0fafc5fbb53c78e820fe362f5972128523959d52
acf-patrick <miharisoap@gmail.com>
2022-03-22 22:11:16 +0300

    property binding

--- src/app/heroes.ts
+++ src/app/heroes.ts
@@ -6,7 +6,7 @@ export const HEROES: Hero[] = [
     { id: 3, name: "Moon Knight" },
     { id: 4, name: "Captain America" },
     { id: 5, name: "Dr. Strange" },
-    { id: 6, name: "Black Panter" },
+    { id: 6, name: "Black Panther" },
     { id: 7, name: "All Might" },
     { id: 8, name: "Batman" },
     { id: 9, name: "Hawk Eye" },
